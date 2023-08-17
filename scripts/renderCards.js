991b99f5e106cfd108e6906ee6d5ce4442b09a93
Yaruto <neo960211@gmail.com>
2023-08-17 17:40:26 +0200

    Added hover to cards

--- scripts/renderCards.js
+++ scripts/renderCards.js
@@ -46,6 +46,27 @@ document.querySelector(".row").innerHTML = data.map(card =>
                 </svg>
             </div>
         </a>
+        <div class="shine"></div>
+        <div class="background">
+        <div class="tiles">
+            <div class="tile tile-1"></div>
+            <div class="tile tile-2"></div>
+            <div class="tile tile-3"></div>
+            <div class="tile tile-4"></div>
+
+            <div class="tile tile-5"></div>
+            <div class="tile tile-6"></div>
+            <div class="tile tile-7"></div>
+            <div class="tile tile-8"></div>
+
+            <div class="tile tile-9"></div>
+            <div class="tile tile-10"></div>
+        </div>
+
+        <div class="line line-1"></div>
+        <div class="line line-2"></div>
+        <div class="line line-3"></div>
+        </div>
         </div>
     </div>`        
 ).join("")
