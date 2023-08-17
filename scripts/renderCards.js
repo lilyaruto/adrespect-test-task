2d740474c0114d70616611100f05266755094222
Yaruto <neo960211@gmail.com>
2023-08-17 16:29:21 +0200

    Added hover to cards buttons

--- scripts/renderCards.js
+++ scripts/renderCards.js
@@ -31,8 +31,21 @@ document.querySelector(".row").innerHTML = data.map(card =>
                 </div>
             </div>
             <a href="#" class="card_btn">
-                Dowiedz się więcej<img class="card_btn_img" src="assets/img/cards-img/arrow.svg" alt="">
-            </a>
+            <span class="mask">
+                <div class="link-container">
+                    <span class="link-title1 title">Dowiedz się więcej</span>
+                    <span class="link-title2 title">Dowiedz się więcej</span>
+                </div>
+            </span>
+            <div class="link-icon">
+                <svg class="icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path id="Vector" d="M7.99528 -1.20755e-06L7.09959 0.895696L13.5769 7.36358L-7.5502e-07 7.36358L-6.43745e-07 8.63642L13.5675 8.63642L7.09959 15.1043L7.99529 16L16 7.99529L7.99528 -1.20755e-06Z" fill="#1B5B31"/>
+                </svg>
+                <svg class="icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path id="Vector" d="M7.99528 -1.20755e-06L7.09959 0.895696L13.5769 7.36358L-7.5502e-07 7.36358L-6.43745e-07 8.63642L13.5675 8.63642L7.09959 15.1043L7.99529 16L16 7.99529L7.99528 -1.20755e-06Z" fill="#1B5B31"/>
+                </svg>
+            </div>
+        </a>
         </div>
     </div>`        
 ).join("")
